9f84e97c7a6d0af5c3a1b1e94f8fbac4e9ea363f
Chris <c.lamothe25@gmail.com>
2018-07-28 21:53:06 -0400

    More Python practice.......numbers and strings

---
+++ basics/escapes.py
@@ -0,0 +1,15 @@
+print ('Can\'t')
+
+print ("I can't \n\neven...")
+
+print("""She said, "I can't 
+
+even..."
+""")
+
+lunch = "Macaroni" + " and meatballs"
+#lunch = lunch + " and salad"
+#or...
+lunch += " and salad"
+lunch += "!" * 25
+print(lunch)
